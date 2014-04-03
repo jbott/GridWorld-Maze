0836dc95db131747c9718fd66e2c66ea114542cb
jbott <snowleopardjb@gmail.com>
2014-04-02 22:29:40 -0400

    Basic player object

--- src/com/mazegame/MazeGame.java
+++ src/com/mazegame/MazeGame.java
@@ -1,35 +1,52 @@
 package com.mazegame;
 
 import info.gridworld.actor.*;
-import info.gridworld.grid.BoundedGrid;
-import info.gridworld.grid.Location;
+import info.gridworld.grid.*;
 
-public class MazeGame
-{
-    public static void main(String[] args)
-    {
+public class MazeGame {
+    public static void main(String[] args) {
         ActorWorld world = new ActorWorld(new BoundedGrid<Actor>(11, 11));
+        world.addOccupantClass(Player.class.getName());
         world.setMessage("Press the arrow keys to move!");
         int[][] rocks = {
                 //0  1  2  3  4  5  6  7  8  9  10
-                { 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 }, // 0
-                { 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0 }, // 1
-                { 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0 }, // 2
-                { 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0 }, // 3
-                { 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1 }, // 4
-                { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, // 5
-                { 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1 }, // 6
-                { 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1 }, // 7
-                { 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // 8
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // 9
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } // 10
+                {1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0}, // 0
+                {0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0}, // 1
+                {0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0}, // 2
+                {0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0}, // 3
+                {0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1}, // 4
+                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 5
+                {0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1}, // 6
+                {0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1}, // 7
+                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // 8
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // 9
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} // 10
         };
-        for(int row = 0; row < rocks.length; row++) {
-            for(int col = 0; col < rocks[0].length; col++) {
+        for (int row = 0; row < rocks.length; row++) {
+            for (int col = 0; col < rocks[0].length; col++) {
                 if (rocks[row][col] == 1)
                     world.add(new Location(row, col), new Rock());
             }
         }
+
+        final Player player = new Player();
+        world.add(new Location(world.getGrid().getNumRows() / 2, world.getGrid().getNumCols() / 2), player);
+
+        java.awt.KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new java.awt.KeyEventDispatcher() {
+            public boolean dispatchKeyEvent(java.awt.event.KeyEvent event) {
+                String key = javax.swing.KeyStroke.getKeyStrokeForEvent(event).toString();
+                if (key.equals("pressed UP"))
+                    player.setDirection(0);
+                if (key.equals("pressed RIGHT"))
+                    player.setDirection(90);
+                if (key.equals("pressed DOWN"))
+                    player.setDirection(180);
+                if (key.equals("pressed LEFT"))
+                    player.setDirection(270);
+                return true;
+            }
+        });
+
         world.show();
     }
 }
---
+++ src/com/mazegame/Player.java
@@ -0,0 +1,70 @@
+package com.mazegame;
+import info.gridworld.actor.*;
+import info.gridworld.grid.*;
+
+public class Player extends Actor {
+    private String imageSuffix = "S";
+
+    Player() {
+        setColor(null);
+    }
+
+    // We do not actually change direction, but rather change which image we display
+    @Override
+    public void setDirection(int dir) {
+        super.setDirection(0);
+        setImageSuffix(dir);
+    }
+
+    public int getImageDirection() {
+        switch (getImageSuffix()) {
+            case "N":
+                return Location.NORTH;
+            case "E":
+                return Location.EAST;
+            default:
+            case "S":
+                return Location.SOUTH;
+            case "W":
+                return Location.WEST;
+        }
+    }
+
+
+    private void setImageSuffix(int direction) {
+        if (direction % 90 != 0)
+            direction += direction % 90;
+        int adjustedDirection = direction % Location.FULL_CIRCLE;
+        if (adjustedDirection < 0)
+            adjustedDirection += Location.FULL_CIRCLE;
+
+        switch(adjustedDirection) {
+            case Location.NORTH:
+                setImageSuffix("N");
+                break;
+            case Location.EAST:
+                setImageSuffix("E");
+                break;
+            default:
+            case Location.SOUTH:
+                setImageSuffix("S");
+                break;
+            case Location.WEST:
+                setImageSuffix("W");
+                break;
+        }
+    }
+
+    @Override
+    public void act() {
+        // Do nothing
+    }
+
+    public String getImageSuffix() {
+        return imageSuffix;
+    }
+
+    public void setImageSuffix(String imageSuffix) {
+        this.imageSuffix = imageSuffix;
+    }
+}
