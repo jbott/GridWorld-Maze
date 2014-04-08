b45c3b2fa127d60432a6afd4c55d5f59637f1126
jbott <snowleopardjb@gmail.com>
2014-04-07 20:37:33 -0400

    Add MazeLoader

--- src/com/mazegame/MazeGame.java
+++ src/com/mazegame/MazeGame.java
@@ -5,49 +5,33 @@ import info.gridworld.grid.*;
 
 public class MazeGame {
     public static void main(String[] args) {
+        // Create root grid
         BoundedGrid<Actor> maze = new BoundedGrid<>(51, 51);
+
         // Load maze into maze grid here
+        MazeLoader mazeLoader = new MazeLoader(MazeGame.class.getResourceAsStream("maze.txt"));
+        mazeLoader.createGrid(maze);
+
+        // Keep references to the important game pieces
+        final Player player = mazeLoader.getPlayer();
+        final Winner win = mazeLoader.getWinner();
+
+        // Initialize the display grid
         if(args.length < 1)
         {
             System.out.println("Call with displayGrid size");
             return;
         }
         final DisplayGrid<Actor> displayGrid = new DisplayGrid<>(maze, Integer.parseInt(args[0]), Integer.parseInt(args[0]));
-        final ActorWorld world = new ActorWorld(displayGrid);
-        world.addOccupantClass(Player.class.getName());
-        world.setMessage("Press the arrow keys to move!");
-        final Player player = new Player();
-		final Winner win = new Winner();
-        int[][] rocks = {
-                //0  1  2  3  4  5  6  7  8  9  10
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, // 0
-                {1, 1, 3, 1, 0, 0, 0, 0, 0, 0, 1}, // 1
-                {1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1}, // 2
-                {1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1}, // 3
-                {1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1}, // 4
-                {1, 1, 0, 0, 0, 2, 0, 0, 0, 0, 1}, // 5
-                {1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1}, // 6
-                {1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1}, // 7
-                {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 8
-                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 9
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1} // 10
-        };
-        for (int row = 0; row < rocks.length; row++) {
-            for (int col = 0; col < rocks[0].length; col++) {
-                if (rocks[row][col] == 1)
-//                  world.add(new Location(row, col), new Rock());
-                (new Rock()).putSelfInGrid(maze, new Location(row, col));
-                else if(rocks[row][col] == 2)
-                    player.putSelfInGrid(maze, new Location(row, col));
-				else if(rocks[row][col] == 3)
-					win.putSelfInGrid(maze, new Location(row, col));
-            }
-        }
-
-        //world.add(new Location(world.getGrid().getNumRows() / 2, world.getGrid().getNumCols() / 2), player);
         displayGrid.setOffset(new Location(player.getLocation().getRow()-displayGrid.getNumRows()/2,
-                                                        player.getLocation().getCol()-displayGrid.getNumCols()/2));
+                player.getLocation().getCol()-displayGrid.getNumCols()/2));
 
+        // Initialize world display
+        final ActorWorld world = new ActorWorld(displayGrid);
+        // Set a nice message
+        world.setMessage("Press the arrow keys to move!");
+
+        // Handle keyboard events
         java.awt.KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new java.awt.KeyEventDispatcher() {
             public boolean dispatchKeyEvent(java.awt.event.KeyEvent event) {
                 String key = javax.swing.KeyStroke.getKeyStrokeForEvent(event).toString();
@@ -62,7 +46,6 @@ public class MazeGame {
                     player.move(dir);
                     displayGrid.setOffset(new Location(player.getLocation().getRow()-displayGrid.getNumRows()/2,
                                                         player.getLocation().getCol()-displayGrid.getNumCols()/2));
-                    //displayGrid.move(dir);
                 }
 				
 				if(win.won())
@@ -72,6 +55,7 @@ public class MazeGame {
             }
         });
 
+        // Make sure to display the image
         world.show();
     }
 }
---
+++ src/com/mazegame/MazeLoader.java
@@ -0,0 +1,68 @@
+/**
+ * @(#)MazeLoader.java
+ *
+ *
+ * @author Will Hamilton
+ * @version 1.00 2014/4/4
+ */
+package com.mazegame;
+
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Rock;
+import info.gridworld.grid.BoundedGrid;
+import info.gridworld.grid.Location;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class MazeLoader {
+    private InputStream file;
+    private Scanner scan;
+    private Player player;
+    private Winner winner;
+
+    public MazeLoader(InputStream file) {
+        this.file = file;
+    }
+
+    public void createGrid(BoundedGrid<Actor> maze) {
+        int row = 0;
+        int col = 0;
+        try {
+
+            scan = new Scanner(file);
+            while (scan.hasNextLine()) {
+
+                String in = scan.nextLine();
+                String[] rocks = in.split(" ");
+                for (col = 0; col < rocks.length; col++) {
+                    if (rocks[col].equals("1")) {
+                        Rock r = new Rock();
+                        r.putSelfInGrid(maze, new Location(row, col));
+                    }
+                    if (rocks[col].equals("2")) {
+                        player = new Player();
+                        player.putSelfInGrid(maze, new Location(row, col));
+                    }
+                    if (rocks[col].equals("3")) {
+                        winner = new Winner();
+                        winner.putSelfInGrid(maze, new Location(row, col));
+                    }
+                }
+                row++;
+                col = 0;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Winner getWinner() {
+        return winner;
+    }
+}
